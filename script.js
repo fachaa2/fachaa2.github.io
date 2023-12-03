df945d2e2c83dccf7832eb06c07c2134a70ef7d2
fachaa2 <fachaa2@hotmail.com>
2023-12-03 01:01:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Agrega aquí la lógica de validación del formulario
+    // Por ejemplo, puedes verificar que los campos no estén vacíos y mostrar mensajes de error si es necesario
+
+    alert('¡Formulario enviado con éxito!');
+});
+
